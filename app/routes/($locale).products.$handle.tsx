ffeb03fcf3a5a6b2b5b75ac7261554aecc324dc7
sam-sapiol-desktop <nkululekovuyo0103@gmail.com>
2026-02-23 20:02:09 +0200

    (Update): adds image carousel to product page and well as modal

--- app/routes/($locale).products.$handle.tsx
+++ app/routes/($locale).products.$handle.tsx
@@ -98,27 +98,23 @@ export default function Product() {
   const {title, descriptionHtml} = product;
 
   return (
-    <div className="product">
-      <ProductImage image={selectedVariant?.image} />
-      <div className="product-main">
-        <h1>{title}</h1>
-        <ProductPrice
-          price={selectedVariant?.price}
-          compareAtPrice={selectedVariant?.compareAtPrice}
-        />
-        <br />
-        <ProductForm
-          productOptions={productOptions}
-          selectedVariant={selectedVariant}
-        />
-        <br />
-        <br />
-        <p>
-          <strong>Description</strong>
-        </p>
-        <br />
-        <div dangerouslySetInnerHTML={{__html: descriptionHtml}} />
-        <br />
+    <div className="pt-48 md:pt-48">
+      <div className="max-w-7xl mx-auto px-4 md:px-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16">
+          {/* Left side Images */}
+          <div className="space-y-8">
+            <ProductImage
+              images={ product.images.nodes.map(node => ({
+                id: node.id,
+                url: node.url,
+                altText: node.altText,
+                width: node.width,
+                height: node.height
+              }))}
+              selectedVariantImage={ selectedVariant.image }
+            />
+          </div>
+        </div>
       </div>
       <Analytics.ProductView
         data={{
@@ -201,6 +197,15 @@ const PRODUCT_FRAGMENT = `#graphql
             }
           }
         }
+      }
+    }
+    images(first: 10) {
+      nodes {
+        id
+        url
+        altText
+        width
+        height
       }
     }
     selectedOrFirstAvailableVariant(selectedOptions: $selectedOptions, ignoreUnknownOptions: true, caseInsensitiveMatch: true) {
